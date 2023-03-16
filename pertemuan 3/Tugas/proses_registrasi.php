f6e86eae09ecb3c25842d466c3c54b75508e93d7
triaamsss <triamaulidasss30@gmail.com>
2023-03-17 00:07:18 +0700

    Membuat form registrasi

--- pertemuan 3/Tugas/proses_registrasi.php
+++ pertemuan 3/Tugas/proses_registrasi.php
@@ -6,6 +6,7 @@ if (isset($_POST['submit'])) {
     $prodi = $_POST['prodi'];
     $skills = $_POST['skill'];
     $domisili = $_POST['domisili'];
+    $email = $_POST['email'];
 
     function skor_skill($skills)
     {
@@ -24,7 +25,26 @@ if (isset($_POST['submit'])) {
         }
         return $result;
     }
-    
+
+    function kategori_skill($skor_skill)
+    {
+        if ($skor_skill == 0) {
+            $predikat = "Tidak Memadai";
+        } elseif ($skor_skill >= 1 and $skor_skill <= 40) {
+            $predikat = "Kurang";
+        } elseif ($skor_skill >= 41 and $skor_skill <= 60) {
+            $predikat = "Cukup";
+        } elseif ($skor_skill >= 61 and $skor_skill <= 100) {
+            $predikat = "Baik";
+        } elseif ($skor_skill >= 101 and $skor_skill <= 150) {
+            $predikat = "Sangat Baik";
+        } else {
+            $predikat = "Eror";
+        }
+
+        return $predikat;
+    }
+
     echo "NIM: $nim";
     echo "<br> Nama: $nama";
     echo "<br> Jenis Kelamin: $gender";
@@ -33,6 +53,10 @@ if (isset($_POST['submit'])) {
     foreach ($skills as $skill) {
         echo $skill . ", ";
     }
-    echo "<br> Tempat Domisili: $domisili";
-    echo "<br> Skor Skill: " . skor_skill($skills);
+    echo "<br> TempatDomisili : $domisili";
+    echo "<br> Skor Skill : " . skor_skill($skills);
+    $nilai = skor_skill($skills);
+    $predikat = kategori_skill($nilai);
+    echo "<br> Kategori Skill : " . $predikat;
+    echo "<br> E-mail : $email";
 }
